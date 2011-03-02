f6d751cf96acccf739952bcf5d471801da3c0a10
tinnguyen <tin.trung.nguyen@techpropulsionlabs.com>
2011-03-02 11:40:55 +0700

    add fetch ga profile

--- db/migrate/035_create_ga_profiles.rb
+++ db/migrate/035_create_ga_profiles.rb
@@ -1,8 +1,12 @@
 class CreateGaProfiles < ActiveRecord::Migration
   def self.up
     create_table :ga_profiles do |t|
-      t.column :content, :text, :null => false
-      t.column :credential, :string, :null => false      
+      t.column :profile_id, :integer, :null => false
+      t.column :title, :string, :null => false
+      t.column :account_id, :integer, :null => false
+      t.column :web_property_id, :string, :null => false
+      t.column :content, :text
+      t.column :credential, :string
       t.column :request_url, :string
       t.column :format, :string
 
--- lib/fetcher/ga/profile.rb
+++ lib/fetcher/ga/profile.rb
@@ -9,10 +9,29 @@ module Fetcher
         accounts = Garb::Management::Account.all
         accounts.each do |account|
           account.profiles.each do |profile|
-            puts profile.id
+            profile_id = profile.id.to_i
+            title = profile.title.to_s
+            account_id =  profile.account_id.to_i
+            web_property_id = profile.web_property_id.to_s
+            puts profile_id
+            puts title
+            puts account_id
+            puts web_property_id
+            begin
+              save_data(profile_id, title, account_id, web_property_id)
+            rescue
+              raise "Data is not fully propered or adequate."
+            end
           end
         end
       end
+
+      def save_data(profile_id, title, account_id, web_property_id)
+        ga_profile =  ::GA::Profile.find_or_initialize_by_profile_id(profile_id)
+        logger.info ga_profile.inspect
+        ga_profile.update_attributes({:profile_id => profile_id, :title => title, :account_id => account_id, :web_property_id => web_property_id})
+      end
+
     end
   end
 end
